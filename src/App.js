d6fdc9b13cf139a0acdc61654caa89d98eabc608
pradeep <pradeep.kumar@qburst.com>
2024-02-02 19:19:56 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const handleClick = async()=>{
+    const result = await axios.get('https://accounts.google.com/o/oauth2/v2/auth?client_id=411042435356-c2le940frh5evk4k7kpikarb89iq98fc.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A3001&response_type=code&scope=openid%20profile%20email&access_type=offline&state=standard_oauth&prompt=consent')
+    console.log(result)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" bg-black p-2 w-max" onClick={handleClick}>
+      <span className='text-white'>google login</span>
     </div>
   );
 }
